42d782f1d7e9a5f65922949862122ce220f9d8f5
sereenaa <serenax8@gmail.com>
2023-11-16 08:03:01 +1100

    delete keys

--- keys.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#keys.py
-
-api = {
-	'key'          : "WDWKHaoHGw5zDxfY2ybjDyP2F",
-	'secret'       : "hKWdM2h6CctU4wiWwMzUW2iORJJBKju04xIjWtN0X2RXrBjjRK",
-	'token'        : "1530950212398632960-b1p60P8aJuaJzuPSbW37A8Ur6KWPPQ",
-	'token_secret' : "lOYTrnCYc9WHzeZBjqUkt4JwQmgM5VWCHMZBsRMKxQsge"
-}
